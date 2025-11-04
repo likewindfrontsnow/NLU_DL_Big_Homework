eb3ba84306f0ba2bf4d31f78de2ddcf18bb4eaf0
likewindfrontsnow <3289074024@qq.com>
2025-11-04 12:03:26 +0800

    change

--- config.py
+++ config.py
@@ -7,6 +7,11 @@ load_dotenv(override=True)
 
 ERNIE_API_KEY = os.getenv("ERNIE_API_KEY")
 
+ERNIE_CONFIG = {
+    "base_url": "https://qianfan.baidubce.com/v2",
+    "api_key": ERNIE_API_KEY,
+    "model": "ernie-3.5-8k" 
+}
 
 if not ERNIE_API_KEY:
     raise ValueError("错误：请在 .env 文件中设置您的 ERNIE_API_KEY")
