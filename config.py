f1f337b2375a132781fa3966ff32247a3fcc8612
likewindfrontsnow <3289074024@qq.com>
2025-11-04 11:24:12 +0800

    just for test

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ from dotenv import load_dotenv
 load_dotenv(override=True)
 
 ERNIE_API_KEY = os.getenv("ERNIE_API_KEY")
+DIFY_API_KEY = os.getenv("DIFY_API_KEY")
 
 
 if not ERNIE_API_KEY:
-    raise ValueError("错误：请在 .env 文件中设置您的 DIFY_API_KEY")
+    raise ValueError("错误：请在 .env 文件中设置您的 ERNIE_API_KEY")
